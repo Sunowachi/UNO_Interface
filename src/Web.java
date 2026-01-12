7517acb2525851759b4bc9bdaafd22ea5289a132
Sunowachi <thedanmaps@gmail.com>
2026-01-12 22:10:09 +0700

    Update Web.java

--- src/Web.java
+++ src/Web.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Web {
 
-    private static final int PORT = 8080;
+    private static final int PORT = 8181;
     public static final long SERVER_START = System.currentTimeMillis();
 
     public static void main(String[] args) throws IOException {
