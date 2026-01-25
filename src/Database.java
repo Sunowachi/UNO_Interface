b434dd60642fa7dacc7e38d01638688aa0c5cf19
Sunowachi <thedanmaps@gmail.com>
2026-01-25 16:17:34 +0700

    1

--- src/Database.java
+++ src/Database.java
@@ -50,13 +50,6 @@ public class Database {
 
     static void init() {
 
-        if (DB_PASS == null || DB_PASS.length() < 4) {
-            System.out.println("DB_PASS=" + System.getenv("DB_PASS"));
-            throw new IllegalStateException(
-                    "DB_PASS must be provided via environment variable"
-            );
-        }
-
         try {
             Class.forName("org.postgresql.Driver");
 
